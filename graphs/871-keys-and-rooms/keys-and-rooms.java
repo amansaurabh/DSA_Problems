2abd8a2323361a3ca779a423dc56dbc038d27d45
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-09-05 08:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (90.84%) - LeetSync

--- graphs/871-keys-and-rooms/keys-and-rooms.java
+++ graphs/871-keys-and-rooms/keys-and-rooms.java
@@ -3,8 +3,8 @@ class Solution {
         int n = rooms.size();
         boolean[] vis = new boolean[n];
         dfs(rooms, 0, vis);
-        for(boolean roomVis : vis){
-             if (!roomVis) {
+        for(int i = 0; i < n; i++){
+             if (!vis[i]) {
                 return false;
             }
         }

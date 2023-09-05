b2e5a1b368c9898d6fdc40cfdcef8b22b3025b4c
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-09-05 09:49:56 +0530

    Time: 9 ms (98.25%) | Memory: 98.9 MB (98.53%) - LeetSync

---
+++ graphs/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int find(int x, int[] parent){
+            if(parent[x] == -1) return x;
+            return parent[x] = find(parent[x], parent);
+        }
+
+    public boolean union(int x, int y, int[] parent){
+            int parent_x = find(x, parent);
+            int parent_y = find(y, parent);
+            if(parent_x == parent_y) return false;
+            parent[parent_x] = parent_y;
+            return true;
+        }
+    
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
+
+        
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            union(u, v, parent);
+        }
+
+        return find(source, parent) == find(destination, parent);
+
+        // we can solve using dfs and bfs also.
+    }
+}

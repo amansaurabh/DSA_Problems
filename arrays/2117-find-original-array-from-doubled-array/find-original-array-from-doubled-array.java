26291f6844a9a35fc4426d838e4053a29c8033f1
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-09-22 14:59:13 +0530

    Time: 92 ms (64.12%) | Memory: 61.5 MB (25.56%) - LeetSync

---
+++ arrays/2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int n = changed.length;
+
+        Map<Integer, Integer> originalSet = new HashMap<>();
+
+        Arrays.sort(changed);
+
+        if(n % 2 != 0){
+            return new int[]{};
+        }
+
+        for (int num : changed) {
+            originalSet.put(num, originalSet.getOrDefault(num, 0) + 1);
+        }
+
+        int[] original = new int[n/2];
+        int index = 0;
+        for (int num : changed) {
+            int twice = 2 * num;
+            if(originalSet.get(num) <= 0) continue;
+            if(originalSet.getOrDefault(twice,0) <= 0){
+                return new int[]{};
+            }
+                original[index] = num;
+                if(index < n - 1){
+                    index++;
+                }
+                originalSet.put(num, originalSet.get(num) - 1);
+                originalSet.put(twice, originalSet.get(twice) - 1);
+        }
+        return original;
+         
+    }
+}

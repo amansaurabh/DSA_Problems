f6c24007bc7074065db1d77a26f0c3e9c7332196
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-09-27 16:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (58.06%) - LeetSync

--- arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,21 +1,57 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int start = 0;
-       int end = nums.length - 1;
-       int si = -1, ei = -1;
-       while(start <= end){
-           if(nums[start] == target){
-               si = start;
-           }else{
-               start++;
-           }
-           if(nums[end] == target){
-               ei = end;
-           }else{
-               end--;
-           }
-           if(si != -1 && ei != -1) return new int[] {si, ei};
-       }
-       return new int[] {si, ei};
+        // my own thinking wala solution
+
+    //    int start = 0;
+    //    int end = nums.length - 1;
+    //    int si = -1, ei = -1;
+    //    while(start <= end){
+    //        if(nums[start] == target){
+    //            si = start;
+    //        }else{
+    //            start++;
+    //        }
+    //        if(nums[end] == target){
+    //            ei = end;
+    //        }else{
+    //            end--;
+    //        }
+    //        if(si != -1 && ei != -1) return new int[] {si, ei};
+    //    }
+    //    return new int[] {si, ei};
+
+
+    //-------------- using BS ------------------------
+
+    int start=0,end=nums.length-1,fi=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                fi=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        start=0;end=nums.length-1;
+        int ei=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+           if(nums[mid]==target){
+                ei=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return new int[]{fi,ei};
     }
 }

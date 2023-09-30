3b355e9e5eafad1430e658bcb214d251a0aef628
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-10-01 00:46:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (48.21%) - LeetSync

--- arrays/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ arrays/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,40 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
+        //----------Linear Search-------
 
-        for(int i = 0 ; i < n; i++){
-            if(nums[i] == target){
-                return i;
+        // for(int i = 0 ; i < n; i++){
+        //     if(nums[i] == target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        //---------Binary Search----------
+        int start = 0;
+        int end = n - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) return mid;
+            //Find out sorted part
+            // 1: left part is sorted
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            //2: Right half is sorted
+            else {
+                if(nums[mid] <= target && target <= nums[end] ){
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
             }
         }
         return -1;

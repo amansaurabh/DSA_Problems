ed921652c994b61668b9e1d9e2b593d58f360608
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-10-04 23:17:46 +0530

    Time: 5 ms (9.79%) | Memory: 40.1 MB (42.70%) - LeetSync

---
+++ binary-search/410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    private boolean isPossible(int[] nums, int subArrSum, int splits){
+        int n = nums.length;
+        int split = 1;
+        long prevSum = 0;
+        for(int i = 0; i < n; i++){
+            if(prevSum + nums[i] <= subArrSum){
+                prevSum += nums[i];
+            }else{
+                split++;
+                prevSum = nums[i];
+            }
+        }
+        return split > splits;
+    }
+    public int splitArray(int[] nums, int k) {
+        
+        int low = Arrays.stream(nums).max().getAsInt();
+        int high = Arrays.stream(nums).sum();
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isPossible(nums, mid, k)){
+                low = mid + 1;
+               
+            }
+            else{
+                high = mid - 1; 
+            }
+        }
+
+        return low;
+    }
+}

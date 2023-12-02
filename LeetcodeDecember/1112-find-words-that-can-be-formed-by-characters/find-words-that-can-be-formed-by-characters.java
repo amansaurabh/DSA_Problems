1b3908a6a75982dcac154de7036ae216af99668b
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-02 19:36:03 +0530

    Time: 50 ms (19.26%) | Memory: 44.3 MB (32.72%) - LeetSync

--- LeetcodeDecember/1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ LeetcodeDecember/1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,25 +1,64 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int[] charCounts = new int[26]; // Assuming lowercase English letters
+        // int[] charCounts = new int[26]; // Assuming lowercase English letters
+        // int totalLength = 0;
+
+        // // Count occurrences of characters in chars
+        // for (char c : chars.toCharArray()) {
+        //     charCounts[c - 'a']++;
+        // }
+
+        // for (String word : words) {
+        //     int[] tempCounts = new int[26];
+        //     boolean canForm = true;
+
+        //     // Count occurrences of characters in the current word
+        //     for (char c : word.toCharArray()) {
+        //         tempCounts[c - 'a']++;
+        //     }
+
+        //     // Check if the word can be formed using characters from chars
+        //     for (int i = 0; i < 26; i++) {
+        //         if (tempCounts[i] > charCounts[i]) {
+        //             canForm = false;
+        //             break;
+        //         }
+        //     }
+
+        //     if (canForm) {
+        //         totalLength += word.length();
+        //     }
+        // }
+
+        // return totalLength;
+
+
+        //---- using Hashmap-----
+
         int totalLength = 0;
+        Map<Character, Integer> charsCount = new HashMap<>();
 
         // Count occurrences of characters in chars
         for (char c : chars.toCharArray()) {
-            charCounts[c - 'a']++;
+            charsCount.put(c, charsCount.getOrDefault(c, 0) + 1);
         }
 
         for (String word : words) {
-            int[] tempCounts = new int[26];
-            boolean canForm = true;
+            Map<Character, Integer> wordCount = new HashMap<>();
 
             // Count occurrences of characters in the current word
             for (char c : word.toCharArray()) {
-                tempCounts[c - 'a']++;
+                wordCount.put(c, wordCount.getOrDefault(c, 0) + 1);
             }
 
+            boolean canForm = true;
+
             // Check if the word can be formed using characters from chars
-            for (int i = 0; i < 26; i++) {
-                if (tempCounts[i] > charCounts[i]) {
+            for (Map.Entry<Character, Integer> entry : wordCount.entrySet()) {
+                char c = entry.getKey();
+                int count = entry.getValue();
+
+                if (!charsCount.containsKey(c) || charsCount.get(c) < count) {
                     canForm = false;
                     break;
                 }

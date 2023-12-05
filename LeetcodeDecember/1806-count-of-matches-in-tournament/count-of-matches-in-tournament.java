18507796d8e9b28aa2733c09c054f644b231bd91
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-05 12:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (40.19%) - LeetSync

---
+++ LeetcodeDecember/1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        
+        //------------O(logn)----------------
+
+        // int matches = 0;
+        // while(n > 1){
+        //     if(n % 2 == 0){
+        //         matches += n / 2;
+        //         n = n / 2;
+        //     }else{
+        //         matches += (n - 1) / 2;
+        //         n = (n-1) / 2 + 1;
+        //     }
+        // }
+        // return matches;
+
+        //-------------O(1)------------------
+        return n - 1;
+    }
+}

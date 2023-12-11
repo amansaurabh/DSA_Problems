4c0518e0645b5850bdb29104ccf6484fb064e738
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-11 14:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (58.02%) - LeetSync

--- LeetcodeDecember/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ LeetcodeDecember/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
 
-        // Approach 1: 
-        int freq = arr.length / 4;
+        //------------------- Approach 1: TC: O(n) AND SC: O(n)-------------------------
+        // int freq = arr.length / 4;
 
-        Map<Integer, Integer> freqMap = new HashMap<>();
+        // Map<Integer, Integer> freqMap = new HashMap<>();
 
-        for(int num : arr){
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-        }
+        // for(int num : arr){
+        //     freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        // }
 
-        for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
-            if(entry.getValue() > freq){
-                return entry.getKey();
-            }
-        }
+        // for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
+        //     if(entry.getValue() > freq){
+        //         return entry.getKey();
+        //     }
+        // }
 
-        return -1;
+        // return -1;
 
         //--------------------- Approach 2: O(n)-----------------------
         // int n = arr.length;
@@ -32,5 +32,66 @@ class Solution {
         //     }
         // }
         // return -1;
+
+        //--------------------- Approach 3: Using Binary Search: O(logn)-----------------------
+        // This approach 3 is discussed by CodestorywithMIK youtube channel
+
+        // TC: of findSpecialInteger() is O(1), as it involves checking only three indices regardless of the array size.
+        // TC: of firstOccur and lastOccur is O(logn)
+        // Hence overall TC is O(logn)
+
+        int n = arr.length;
+
+        int freq = n / 4;
+
+        List<Integer> candidates = Arrays.asList(arr[n / 4], arr[n / 2], arr[3 * n / 4]);
+
+        for (int candidate : candidates) {
+            int left = firstOccur(candidate, 0, n - 1, arr);
+            int right = lastOccur(candidate, 0, n - 1, arr);
+
+            if (right - left + 1 > freq) {
+                return candidate;
+            }
+        }
+
+        return -1;
+    }
+     private int firstOccur(int target, int l, int r, int[] arr) {
+        int result = -1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                r = mid - 1;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return result;
+    }
+    // Binary search to find the last occurrence of an element in the array
+    private int lastOccur(int target, int l, int r, int[] arr) {
+        int result = -1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                l = mid + 1;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return result;
     }
 }

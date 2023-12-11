4f942d42c2d77d3903868ff2e440989665fe7908
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-11 14:27:09 +0530

    Time: 11 ms (24.41%) | Memory: 44.6 MB (9.34%) - LeetSync

--- LeetcodeDecember/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ LeetcodeDecember/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -1,17 +1,36 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        int n = arr.length;
-        int freq = n / 4;
 
-        if(n == 1){
-            return arr[0];
+        // Approach 1: 
+        int freq = arr.length / 4;
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for(int num : arr){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        for(int i = 0; i < n - freq; i++){
-            if(arr[i] == arr[i + freq]){
-                return arr[i];
+        for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
+            if(entry.getValue() > freq){
+                return entry.getKey();
             }
         }
+
         return -1;
+
+        //--------------------- Approach 2: O(n)-----------------------
+        // int n = arr.length;
+        // int freq = n / 4;
+
+        // if(n == 1){
+        //     return arr[0];
+        // }
+
+        // for(int i = 0; i < n - freq; i++){
+        //     if(arr[i] == arr[i + freq]){
+        //         return arr[i];
+        //     }
+        // }
+        // return -1;
     }
 }

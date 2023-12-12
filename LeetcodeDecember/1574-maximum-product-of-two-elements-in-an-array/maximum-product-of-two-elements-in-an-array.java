dc6997d236f4a64cff550031b505f848152f5507
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-12 18:35:20 +0530

    Time: 1 ms (78.72%) | Memory: 41.9 MB (40.38%) - LeetSync

--- LeetcodeDecember/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ LeetcodeDecember/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,17 +1,36 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = nums[0];
-        int ans = 0;
-        for(int i = 1; i < nums.length; i++){
-            int product = (max - 1) * (nums[i] - 1); 
-            if(ans < product){
-                ans = product;
-            }
-            if(max < nums[i]){
-                max = nums[i];
-            }
+
+        //-----------------Approach 1: --------------------------
+
+        // int max = nums[0];
+        // int ans = 0;
+        // for(int i = 1; i < nums.length; i++){
+        //     int product = (max - 1) * (nums[i] - 1); 
+        //     if(ans < product){
+        //         ans = product;
+        //     }
+        //     if(max < nums[i]){
+        //         max = nums[i];
+        //     }
             
+        // }
+        // return ans;
+
+        //-----------------Approach 2: --------------------------
+
+        int largest = 0;
+        int sec_largest = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > largest){
+                sec_largest = largest;
+                largest = nums[i];
+            }
+            else{
+                sec_largest = Math.max(sec_largest, nums[i]);
+            }
         }
-        return ans;
+        return (sec_largest- 1) * (largest - 1);
     }
 }

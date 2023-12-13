89df553efc15b95a89197b730821e8fda82abf01
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-13 11:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.47%) - LeetSync

--- LeetcodeDecember/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ LeetcodeDecember/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,7 +1,38 @@
 class Solution {
     public int numSpecial(int[][] mat) {
-        // 
         
+    // -------------------- Approach 1:-------------------------
+
+        // int rows = mat.length;
+        // int cols = mat[0].length;
+        // int[] rowCount = new int[rows];
+        // int[] colCount = new int[cols];
+        // int result = 0;
+
+        // // Count occurrences of 1s in rows and columns
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         if (mat[i][j] == 1) {
+        //             rowCount[i]++;
+        //             colCount[j]++;
+        //         }
+        //     }
+        // }
+
+        // // Check for special positions
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
+        //             result++;
+        //         }
+        //     }
+        // }
+
+        // return result;
+
+
+    // -------------------- Approach 1:-------------------------
+
         int specials = 0;
 
         for (int i = 0; i < mat.length; i++) {

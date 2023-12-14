380979af2708af3fc659e5235bf68875dd156dc6
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-14 20:03:17 +0530

    Time: 9 ms (70.38%) | Memory: 66.6 MB (20.89%) - LeetSync

--- Recursion/2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ Recursion/2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -27,8 +27,42 @@ class Solution {
         // head.next = curr;
         // return temp;
         
-        if (head == null) return null;
-        head.next = removeNodes(head.next);
-        return head.next != null && head.val < head.next.val ? head.next : head;
+        // if (head == null) return null;
+        // head.next = removeNodes(head.next);
+        // return head.next != null && head.val < head.next.val ? head.next : head;
+        
+        // ------------------------Approach 2: ----------------------
+        ListNode cur = head;
+        List<ListNode> list = new ArrayList<>();
+        while (cur != null) {
+            list.add(cur);
+            cur = cur.next;
+        }
+        int max = 0;
+        int[] arr = new int[list.size()];
+        //finding max from the end to include values into result
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (max <= list.get(i).val) {
+                arr[i] = 1;
+                max = list.get(i).val;
+            }
+        }
+        boolean headSet = false;
+        cur = head;
+        //adding nodes to the result
+        for (int i = 0; i < list.size(); i++) {
+            if (arr[i] == 1) {
+                if (!headSet) {
+                    headSet = true;
+                    head = list.get(i);
+                    cur = head;
+                } else {
+                    cur.next = list.get(i);
+                    cur = cur.next;
+                }
+            }
+        }
+        cur.next = null;
+        return head;
     }
 }

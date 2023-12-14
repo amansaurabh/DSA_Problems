e2077fc903a2eb09b6592014b9d3e1a31ac18510
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-14 11:45:45 +0530

    Time: 5 ms (92.56%) | Memory: 73.3 MB (44.66%) - LeetSync

--- LeetcodeDecember/2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ LeetcodeDecember/2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -15,17 +15,28 @@ class Solution {
             }
         }
         
-        int[][] diff = new int[m][n];
+        
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                int onesRowi = rowOnes[i];
-                int onesColj = colOnes[j];
-                int zerosRowi = m - rowOnes[i];
-                int zerosColj = n - colOnes[j];
-                diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj;
+        // int[][] diff = new int[m][n];
+
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         int onesRowi = rowOnes[i];
+        //         int onesColj = colOnes[j];
+        //         int zerosRowi = m - rowOnes[i];
+        //         int zerosColj = n - colOnes[j];
+        //         diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj;
+        //     }
+        // }
+        // return diff;    
+
+        // Approach 2: Without using extra space
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                grid[i][j] = 2 * (rowOnes[i] + colOnes[j]) - m - n;
             }
         }
-        return diff;    
+
+        return grid;
     }
 }

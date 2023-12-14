48900cd7fbde0b291fee709c3cf20d29ee477b4e
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-14 12:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (65.94%) - LeetSync

---
+++ Recursion/1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int[] lettercount = new int[26];
+        for(char letter : letters){
+            lettercount[letter - 'a']++;
+        }
+        return backtrack(words, lettercount, score, 0);
+    }
+    private int backtrack(String[] words, int[] lettercount, int[] score, int idx){
+        if(idx == words.length){
+            return 0;
+        }
+        // Every word has two choice : Yes Or No
+        // if that word dont want to join then
+        int scoreWithoutWord = backtrack(words, lettercount, score, idx + 1);
+
+        int sword = 0;
+        String word = words[idx];
+        boolean flag = true;
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+
+            if(lettercount[ch - 'a'] == 0){
+                flag = false;
+                
+            }
+            lettercount[ch - 'a']--;
+            sword += score[ch - 'a'];
+        }
+
+        // Calculate score using the current word if possible
+        int scoreWithWord = 0;
+        if(flag){
+            scoreWithWord += sword + backtrack(words, lettercount, score, idx + 1);
+        }
+
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            lettercount[ch - 'a']++;
+        }
+        // Return the maximum score between using and not using the current word
+        return Math.max(scoreWithWord, scoreWithoutWord);
+    }
+}

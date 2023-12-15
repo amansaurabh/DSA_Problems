eb2342f788203edfff88a9c1b95aaa9d06fd8db1
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-15 10:58:43 +0530

    Time: 2 ms (79.52%) | Memory: 43.7 MB (7.38%) - LeetSync

--- LeetcodeDecember/1547-destination-city/destination-city.java
+++ LeetcodeDecember/1547-destination-city/destination-city.java
@@ -1,20 +1,40 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        Map<String, String> map = new HashMap<>();
 
-        // Build the map with paths, where key = starting city, value = destination city
+        
+        // Map<String, String> map = new HashMap<>();
+
+        // // Build the map with paths, where key = starting city, value = destination city
+        // for (List<String> path : paths) {
+        //     String start = path.get(0);
+        //     String dest = path.get(1);
+        //     map.put(start, dest);
+        // }
+
+        // // Find the destination city by following the paths until a city with no outgoing path is found
+        // String currentCity = paths.get(0).get(0); // Start from any city
+        // while (map.containsKey(currentCity)) {
+        //     currentCity = map.get(currentCity);
+        // }
+
+        // return currentCity;
+
+        Set<String> startingCities = new HashSet<>();
+        Set<String> destinationCities = new HashSet<>();
+
+        // Store all starting cities and destination cities in respective sets
         for (List<String> path : paths) {
-            String start = path.get(0);
-            String dest = path.get(1);
-            map.put(start, dest);
+            startingCities.add(path.get(0));
+            destinationCities.add(path.get(1));
         }
 
-        // Find the destination city by following the paths until a city with no outgoing path is found
-        String currentCity = paths.get(0).get(0); // Start from any city
-        while (map.containsKey(currentCity)) {
-            currentCity = map.get(currentCity);
+        // Find the destination city by checking cities with no outgoing paths
+        for (String city : destinationCities) {
+            if (!startingCities.contains(city)) {
+                return city;
+            }
         }
 
-        return currentCity;
+        return "";
     }
 }

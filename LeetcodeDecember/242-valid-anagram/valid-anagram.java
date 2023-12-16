98035dd063f167d92be59a8db4dfa4be8b871bd5
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-16 11:00:08 +0530

    Time: 15 ms (27.65%) | Memory: 43.5 MB (48.93%) - LeetSync

--- LeetcodeDecember/242-valid-anagram/valid-anagram.java
+++ LeetcodeDecember/242-valid-anagram/valid-anagram.java
@@ -3,15 +3,29 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        int[] count = new int[128];
+        // int[] count = new int[128];
 
+        // for(char ch : s.toCharArray()){
+        //     count[ch - 'a']++;
+        // } 
+
+        // for(char ch : t.toCharArray()){
+        //     if(--count[ch - 'a'] < 0) return false;
+        // } 
+        // return true;
+        HashMap<Character, Integer> charFreq = new HashMap<>();
         for(char ch : s.toCharArray()){
-            count[ch - 'a']++;
-        } 
-
+            charFreq.put(ch, charFreq.getOrDefault(ch, 0) + 1);
+        }
         for(char ch : t.toCharArray()){
-            if(--count[ch - 'a'] < 0) return false;
-        } 
+            if(!charFreq.containsKey(ch)) return false;
+            else{
+                charFreq.put(ch, charFreq.get(ch) - 1);
+                if(charFreq.get(ch) < 0){
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }

91a0535e595453c6aabc8eec31a38f3ca9519736
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-16 10:28:26 +0530

    Time: 2 ms (98.39%) | Memory: 43.7 MB (32.66%) - LeetSync

---
+++ LeetcodeDecember/242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+
+        for(char ch : s.toCharArray()){
+            count[ch - 'a']++;
+        } 
+
+        for(char ch : t.toCharArray()){
+            if(--count[ch - 'a'] < 0) return false;
+        } 
+        return true;
+    }
+}

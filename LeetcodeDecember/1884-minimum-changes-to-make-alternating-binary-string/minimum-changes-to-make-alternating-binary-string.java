38eda54ad0c31435e1c5ee4f7e8350095bcda6c9
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-24 12:09:50 +0530

    Time: 7 ms (9.51%) | Memory: 43 MB (5.57%) - LeetSync

--- LeetcodeDecember/1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ LeetcodeDecember/1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -3,23 +3,19 @@ class Solution {
         int operationsWithZeroFirst = 0;
         int operationsWithOneFirst = 0;
 
-        // Calculate the number of operations if the first character is '0'
         for (int i = 0; i < s.length(); i++) {
-            char expected = (i % 2 == 0) ? '0' : '1';
-            if (s.charAt(i) != expected) {
+            char expectedZero = (i % 2 == 0) ? '0' : '1';
+            char expectedOne = (i % 2 == 0) ? '1' : '0';
+
+            if (s.charAt(i) != expectedZero) {
                 operationsWithZeroFirst++;
             }
-        }
 
-        // Calculate the number of operations if the first character is '1'
-        for (int i = 0; i < s.length(); i++) {
-            char expected = (i % 2 == 0) ? '1' : '0';
-            if (s.charAt(i) != expected) {
+            if (s.charAt(i) != expectedOne) {
                 operationsWithOneFirst++;
             }
         }
 
-        // Return the minimum number of operations needed
         return Math.min(operationsWithZeroFirst, operationsWithOneFirst);
     }
 }

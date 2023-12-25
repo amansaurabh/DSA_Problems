f796933652e7c58f4fc8204be04c2ae4525d4beb
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-25 12:14:42 +0530

    Time: 2 ms (68.78%) | Memory: 45.9 MB (6.11%) - LeetSync

---
+++ arrays/283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0; // Left pointer to track non-zero elements
+
+        // Iterate through the array
+        for (int right = 0; right < nums.length; right++) {
+            // If the current element is non-zero, swap it with the left pointer element
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++; // Move the left pointer to the next position
+            }
+        }
+    }
+}

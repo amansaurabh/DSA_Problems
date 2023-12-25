ae2c350d96fc6b042ac645274c3a241332e5dfd1
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-25 12:20:29 +0530

    Time: 2 ms (26.23%) | Memory: 41.3 MB (10.41%) - LeetSync

---
+++ arrays/392-is-subsequence/is-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sIndex = 0; // Pointer for string s
+        int tIndex = 0; // Pointer for string t
+
+        // Loop through both strings
+        while (sIndex < s.length() && tIndex < t.length()) {
+            // If characters match, move sIndex to the next character in s
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
+            }
+            // Move tIndex to the next character in t in any case
+            tIndex++;
+        }
+
+        // If sIndex reaches the end of s, then all characters in s were found in t in order
+        return sIndex == s.length();
+    }
+}

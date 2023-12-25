cc4b842359b5944fbfff42df7e32f948d0ce3885
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-25 12:16:08 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (6.11%) - LeetSync

--- arrays/283-move-zeroes/move-zeroes.java
+++ arrays/283-move-zeroes/move-zeroes.java
@@ -1,16 +1,35 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0; // Left pointer to track non-zero elements
+        // int left = 0; // Left pointer to track non-zero elements
 
-        // Iterate through the array
-        for (int right = 0; right < nums.length; right++) {
-            // If the current element is non-zero, swap it with the left pointer element
-            if (nums[right] != 0) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++; // Move the left pointer to the next position
+        // // Iterate through the array
+        // for (int right = 0; right < nums.length; right++) {
+        //     // If the current element is non-zero, swap it with the left pointer element
+        //     if (nums[right] != 0) {
+        //         int temp = nums[left];
+        //         nums[left] = nums[right];
+        //         nums[right] = temp;
+        //         left++; // Move the left pointer to the next position
+        //     }
+        // }
+
+        if (nums.length == 1) {
+            return;
+        }
+
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
             }
+        }
+
+       
+         while(nonZeroIndex<nums.length){
+            nums[nonZeroIndex] = 0;
+            nonZeroIndex++;
         }
     }
 }

dbe56d7857f270f539f7277e7a8ef3ea0b60ba17
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-27 19:23:45 +0530

    Time: 4 ms (9.01%) | Memory: 41.6 MB (7.16%) - LeetSync

--- arrays/1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ arrays/1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,17 +1,30 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        int n = arr.length;
+        // int n = arr.length;
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        for(int el : arr){
-            if(map.containsKey(el)){
-                map.put(el, map.get(el) + 1);
-            }
-            else{
-                map.put(el, map.getOrDefault(el, 0) + 1);
-            }
+        // for(int el : arr){
+        //     if(map.containsKey(el)){
+        //         map.put(el, map.get(el) + 1);
+        //     }
+        //     else{
+        //         map.put(el, map.getOrDefault(el, 0) + 1);
+        //     }
+        // }
+        // return map.size() == new HashSet<>(map.values()).size();
+
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        // Count the frequency of occurrences of each number in the array
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
-        return map.size() == new HashSet<>(map.values()).size();
+
+        Set<Integer> frequencySet = new HashSet<>(frequencyMap.values());
+
+        // If the size of the set matches the number of unique frequencies, return true; otherwise, return false
+        return frequencySet.size() == frequencyMap.size();
     }
 }

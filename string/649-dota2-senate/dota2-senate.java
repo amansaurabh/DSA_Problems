ab5d710ac4cb1d2e1a4645df6b2e23a0d98d7a69
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-30 20:25:29 +0530

    Time: 21 ms (13.55%) | Memory: 45.9 MB (5.81%) - LeetSync

--- string/649-dota2-senate/dota2-senate.java
+++ string/649-dota2-senate/dota2-senate.java
@@ -1,37 +1,60 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-         int n = senate.length();
-        int[] bans = new int[2]; // Index 0 for Radiant, Index 1 for Dire
-        int[] senators = new int[2]; // Tracks the count of senators for each party still holding voting rights
+        // int n = senate.length();
+        // int[] bans = new int[2]; // Index 0 for Radiant, Index 1 for Dire
+        // int[] senators = new int[2]; // Tracks the count of senators for each party still holding voting rights
 
-        for (char party : senate.toCharArray()) {
-            if (party == 'R') senators[0]++;
-            else senators[1]++;
+        // for (char party : senate.toCharArray()) {
+        //     if (party == 'R') senators[0]++;
+        //     else senators[1]++;
+        // }
+
+        // while (senators[0] > 0 && senators[1] > 0) {
+        //     for (int i = 0; i < n; i++) {
+        //         char party = senate.charAt(i);
+        //         if (party == 'R') {
+        //             if (bans[0] > 0) {
+        //                 bans[0]--;
+        //                 senators[0]--;
+        //                 senate = senate.substring(0, i) + "-" + senate.substring(i + 1);
+        //             } else {
+        //                 bans[1]++;
+        //             }
+        //         } else if (party == 'D') {
+        //             if (bans[1] > 0) {
+        //                 bans[1]--;
+        //                 senators[1]--;
+        //                 senate = senate.substring(0, i) + "-" + senate.substring(i + 1);
+        //             } else {
+        //                 bans[0]++;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return senators[0] > 0 ? "Radiant" : "Dire";
+
+
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+        int n = senate.length();
+
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') radiant.offer(i);
+            else dire.offer(i);
         }
 
-        while (senators[0] > 0 && senators[1] > 0) {
-            for (int i = 0; i < n; i++) {
-                char party = senate.charAt(i);
-                if (party == 'R') {
-                    if (bans[0] > 0) {
-                        bans[0]--;
-                        senators[0]--;
-                        senate = senate.substring(0, i) + "-" + senate.substring(i + 1);
-                    } else {
-                        bans[1]++;
-                    }
-                } else if (party == 'D') {
-                    if (bans[1] > 0) {
-                        bans[1]--;
-                        senators[1]--;
-                        senate = senate.substring(0, i) + "-" + senate.substring(i + 1);
-                    } else {
-                        bans[0]++;
-                    }
-                }
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+            int radiantIndex = radiant.poll();
+            int direIndex = dire.poll();
+
+            if (radiantIndex < direIndex) {
+                radiant.offer(radiantIndex + n);
+            } else {
+                dire.offer(direIndex + n);
             }
         }
 
-        return senators[0] > 0 ? "Radiant" : "Dire";
+        return radiant.isEmpty() ? "Dire" : "Radiant";
     }
 }

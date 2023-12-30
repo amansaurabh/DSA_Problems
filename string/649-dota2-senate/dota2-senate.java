af5f859a9de6878729b14af25cd35785ec85c717
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-30 20:24:49 +0530

    Time: 184 ms (5.00%) | Memory: 45.5 MB (9.86%) - LeetSync

---
+++ string/649-dota2-senate/dota2-senate.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+         int n = senate.length();
+        int[] bans = new int[2]; // Index 0 for Radiant, Index 1 for Dire
+        int[] senators = new int[2]; // Tracks the count of senators for each party still holding voting rights
+
+        for (char party : senate.toCharArray()) {
+            if (party == 'R') senators[0]++;
+            else senators[1]++;
+        }
+
+        while (senators[0] > 0 && senators[1] > 0) {
+            for (int i = 0; i < n; i++) {
+                char party = senate.charAt(i);
+                if (party == 'R') {
+                    if (bans[0] > 0) {
+                        bans[0]--;
+                        senators[0]--;
+                        senate = senate.substring(0, i) + "-" + senate.substring(i + 1);
+                    } else {
+                        bans[1]++;
+                    }
+                } else if (party == 'D') {
+                    if (bans[1] > 0) {
+                        bans[1]--;
+                        senators[1]--;
+                        senate = senate.substring(0, i) + "-" + senate.substring(i + 1);
+                    } else {
+                        bans[0]++;
+                    }
+                }
+            }
+        }
+
+        return senators[0] > 0 ? "Radiant" : "Dire";
+    }
+}

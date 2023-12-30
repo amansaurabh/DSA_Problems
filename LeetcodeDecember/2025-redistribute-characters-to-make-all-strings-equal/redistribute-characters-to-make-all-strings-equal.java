9e3453d6ea2427662f11dd9d01fee74ce1bfe1a3
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-30 10:22:43 +0530

    Time: 4 ms (43.02%) | Memory: 43.8 MB (9.06%) - LeetSync

--- LeetcodeDecember/2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
+++ LeetcodeDecember/2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -1,19 +1,37 @@
 class Solution {
     public boolean makeEqual(String[] words) {
-        Map<Character, Integer> mp = new HashMap<>();
-        for(String str : words){
-            for(char ch : str.toCharArray()){
-                mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-            }
-        }
+        // Map<Character, Integer> mp = new HashMap<>();
+        // for(String str : words){
+        //     for(char ch : str.toCharArray()){
+        //         mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        //     }
+        // }
 
-        int n = words.length;
+        // int n = words.length;
 
-        for(int count : mp.values()){
-            if(count % n != 0){
-                return false;
-            }
+        // for(int count : mp.values()){
+        //     if(count % n != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+    int[] charCount = new int[26]; // only lowercase English letters
+
+    // Count occurrences of each character in all words
+    for (String word : words) {
+        for (char ch : word.toCharArray()) {
+            charCount[ch - 'a']++;
         }
-        return true;
+    }
+
+    int n = words.length;
+    for (int count : charCount) {
+        if (count % n != 0) {
+            return false; // If any character count is not divisible by the total number of words
+        }
+    }
+
+    return true;
     }
 }

fefb102ff7cc668290cdf3e354f42527678bd5dd
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-30 23:19:25 +0530

    Time: 7 ms (48.48%) | Memory: 57 MB (95.50%) - LeetSync

--- linkedList/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ linkedList/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -28,22 +28,59 @@ class Solution {
         // return maxPair;
 
         // int[] arr = new int[100000];
-        ArrayList<Integer> al = new ArrayList<>();
+        // ArrayList<Integer> al = new ArrayList<>();
 
-        while(head!=null){
-            al.add(head.val);
-            head = head.next;
-        }
-        //initialize the maxSum and traverse half through the array
-        int maxSum = 0;
-        int index = al.size();
-        for(int i=0;i<index/2;i++){
-            //calculate twin sum
-            int twinSum = al.get(i)+al.get(index-1-i);
+        // while(head!=null){
+        //     al.add(head.val);
+        //     head = head.next;
+        // }
+        // //initialize the maxSum and traverse half through the array
+        // int maxSum = 0;
+        // int index = al.size();
+        // for(int i=0;i<index/2;i++){
+        //     //calculate twin sum
+        //     int twinSum = al.get(i)+al.get(index-1-i);
 
-            //update the max Sum
-            maxSum = Math.max(maxSum, twinSum);
+        //     //update the max Sum
+        //     maxSum = Math.max(maxSum, twinSum);
+        // }
+        // return maxSum;
+
+
+         // Find the midpoint of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return maxSum;
+
+        
+        ListNode mid = slow;
+
+        // Reverse the second half of the linked list
+        ListNode prev = null;
+        ListNode next = null;
+        while (mid != null) {
+            next = mid.next;
+            mid.next = prev;
+            prev = mid;
+            mid = next;
+        }
+
+        // Find the maximum twin sum
+        int maxTwinSum = Integer.MIN_VALUE;
+        ListNode current1 = head;
+        ListNode current2 = prev;
+
+        while (current2 != null) {
+            int twinSum = current1.val + current2.val;
+            maxTwinSum = Math.max(maxTwinSum, twinSum);
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+
+        return maxTwinSum;
     }
 }

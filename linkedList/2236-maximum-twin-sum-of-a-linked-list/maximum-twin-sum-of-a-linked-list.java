af45821977a29dfd17daa50d537f576d4bbbdf17
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-30 23:01:20 +0530

    Time: 11 ms (26.37%) | Memory: 56.4 MB (98.35%) - LeetSync

--- linkedList/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ linkedList/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,21 +10,38 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        ListNode temp = head;
-        Stack<Integer> stk = new Stack<>();
-        while(temp != null){
-            stk.push(temp.val);
-            temp = temp.next;
-        }
+        // ListNode temp = head;
+        // Stack<Integer> stk = new Stack<>();
+        // while(temp != null){
+        //     stk.push(temp.val);
+        //     temp = temp.next;
+        // }
 
-        int maxPair = Integer.MIN_VALUE;
-        int stkSize = stk.size();
-        temp = head;
-        for(int i = 0; i < stkSize / 2; i++){
-            int sum = temp.val + stk.pop();
-            maxPair = Math.max(maxPair, sum);
-            temp = temp.next;
-        } 
-        return maxPair;
+        // int maxPair = Integer.MIN_VALUE;
+        // int stkSize = stk.size();
+        // temp = head;
+        // for(int i = 0; i < stkSize / 2; i++){
+        //     int sum = temp.val + stk.pop();
+        //     maxPair = Math.max(maxPair, sum);
+        //     temp = temp.next;
+        // } 
+        // return maxPair;
+
+        int[] arr = new int[100000];
+        int index = 0;
+        while(head!=null){
+            arr[index++] = head.val;
+            head = head.next;
+        }
+        //initialize the maxSum and traverse half through the array
+        int maxSum = 0;
+        for(int i=0;i<index/2;i++){
+            //calculate twin sum
+            int twinSum = arr[i]+arr[index-1-i];
+
+            //update the max Sum
+            maxSum = Math.max(maxSum, twinSum);
+        }
+        return maxSum;
     }
 }

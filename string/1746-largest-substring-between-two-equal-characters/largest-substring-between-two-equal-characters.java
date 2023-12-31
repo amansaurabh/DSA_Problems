9814bf127d3e22f6f4afce8094cda1ac9eb0bde5
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2023-12-31 11:06:09 +0530

    Time: 1 ms (86.22%) | Memory: 41 MB (23.06%) - LeetSync

--- string/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ string/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -1,16 +1,36 @@
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
-        int n = s.length();
-        Map<Character, Integer> charIndices = new HashMap<>();
-        int maxLength = -1;
+        // int n = s.length();
+        // Map<Character, Integer> charIndices = new HashMap<>();
+        // int maxLength = -1;
 
-        for (int i = 0; i < n; i++) {
+        // for (int i = 0; i < n; i++) {
+        //     char ch = s.charAt(i);
+        //     if (charIndices.containsKey(ch)) {
+        //         int prevIndex = charIndices.get(ch);
+        //         maxLength = Math.max(maxLength, i - prevIndex - 1);
+        //     } else {
+        //         charIndices.put(ch, i);
+        //     }
+        // }
+
+        // return maxLength;
+
+
+        int maxLength = -1;
+        int[] indices = new int[26]; // Assuming lowercase English letters
+
+        // Initialize the indices array with -1 (unvisited)
+        for (int i = 0; i < 26; i++) {
+            indices[i] = -1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (charIndices.containsKey(ch)) {
-                int prevIndex = charIndices.get(ch);
-                maxLength = Math.max(maxLength, i - prevIndex - 1);
+            if (indices[ch - 'a'] == -1) {
+                indices[ch - 'a'] = i; // Store the first occurrence index of the character
             } else {
-                charIndices.put(ch, i);
+                maxLength = Math.max(maxLength, i - indices[ch - 'a'] - 1);
             }
         }
 

e2ccffa052e6f5f5d678135d318b0ee083552be3
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-01 17:57:45 +0530

    Time: 3 ms (37.78%) | Memory: 45.8 MB (15.27%) - LeetSync

--- bit-manipulation/136-single-number/single-number.java
+++ bit-manipulation/136-single-number/single-number.java
@@ -1,10 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor = nums[0];
-        int n = nums.length;
-        for(int i = 1; i < n; i++){
-            xor ^= nums[i];
+        // int xor = nums[0];
+        // int n = nums.length;
+        // for(int i = 1; i < n; i++){
+        //     xor ^= nums[i];
+        // }
+        // return xor;
+
+        // other way
+        int n = nums.length; // extracting the size of the array
+        
+        // traverse from the array
+        for(int i = 0; i < n - 1; i++)
+        {
+            nums[i + 1] = nums[i] ^ nums[i + 1]; // (prev answer xor current index)
         }
-        return xor;
+        
+        return nums[n- 1]; // return left over element
     }
 }

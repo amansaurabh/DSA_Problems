05adba480088b6e1c959f6a3b87220ba461c733b
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-06 16:12:05 +0530

    Time: 31 ms (74.68%) | Memory: 56 MB (45.79%) - LeetSync

--- LeetcodeJan/1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ LeetcodeJan/1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,18 +1,57 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        // int n = startTime.length;
+        // int[][] jobs = new int[n][3];
+        // for (int i = 0; i < n; i++) {
+        //     jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+        // }
+        // Arrays.sort(jobs, (a, b)->a[1] - b[1]);
+        // TreeMap<Integer, Integer> dp = new TreeMap<>();
+        // dp.put(0, 0);
+        // for (int[] job : jobs) {
+        //     int cur = dp.floorEntry(job[0]).getValue() + job[2];
+        //     if (cur > dp.lastEntry().getValue())
+        //         dp.put(job[1], cur);
+        // }
+        // return dp.lastEntry().getValue();
+
         int n = startTime.length;
         int[][] jobs = new int[n][3];
+
         for (int i = 0; i < n; i++) {
-            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
-        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-        dp.put(0, 0);
-        for (int[] job : jobs) {
-            int cur = dp.floorEntry(job[0]).getValue() + job[2];
-            if (cur > dp.lastEntry().getValue())
-                dp.put(job[1], cur);
+
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a[1]));
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int currProfit = jobs[i - 1][2];
+            int prevIdx = binarySearch(jobs, i - 1);
+
+            dp[i] = Math.max(currProfit + dp[prevIdx + 1], dp[i - 1]);
         }
-        return dp.lastEntry().getValue();
+
+        return dp[n];
+    }
+
+    private int binarySearch(int[][] jobs, int i) {
+        int low = 0, high = i - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (jobs[mid][1] <= jobs[i][0]) {
+                if (jobs[mid + 1][1] <= jobs[i][0]) {
+                    low = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }

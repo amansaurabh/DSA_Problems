b3a410d2649ef0253908aa91c130b514bf0c3010
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-07 12:19:34 +0530

    Time: 86 ms (22.88%) | Memory: 63.8 MB (16.54%) - LeetSync

---
+++ heap/2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        int[][] arr = new int[n][2];
+
+        for (int i=0; i<n; i++)
+            arr[i] = new int[] {nums1[i], nums2[i]};
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(b[1], a[1]));
+
+        long ans = 0, sum = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int i=0; i<n; i++) {
+        heap.offer(arr[i][0]);
+        sum += arr[i][0];
+
+        if (i >= k)
+            sum -= heap.poll();
+        
+        if (i >= k-1)
+            ans = Math.max(ans, sum * arr[i][1]);
+        }
+        return ans;
+    }
+}

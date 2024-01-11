4d6f9fb1f2020ecc4326df061d04b50b0413d511
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-11 12:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (32.81%) - LeetSync

--- BinaryTree/1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ BinaryTree/1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -14,33 +14,52 @@
  * }
  */
 
-class Solution {
-    int maxDiff = -1;
-    void findMaxUtil(TreeNode root, TreeNode child){
-        if(root == null || child == null){
-            return;
-        }
-        maxDiff = Math.max(maxDiff, Math.abs(root.val - child.val));
 
-        findMaxUtil(root, child.left);
-        findMaxUtil(root, child.right);
+// --------------------- Two Pass solution -----------------------
 
+// class Solution {
+//     int maxDiff = -1;
+//     void findMaxUtil(TreeNode root, TreeNode child){
+//         if(root == null || child == null){
+//             return;
+//         }
+//         maxDiff = Math.max(maxDiff, Math.abs(root.val - child.val));
+
+//         findMaxUtil(root, child.left);
+//         findMaxUtil(root, child.right);
+
+//     }
+
+//     void findMaxDiff(TreeNode root){
+//         if(root == null) return;
+
+//         findMaxUtil(root, root.left);
+//         findMaxUtil(root, root.right);
+
+//         findMaxDiff(root.left);
+//         findMaxDiff(root.right);
+
+//     } 
+
+//     public int maxAncestorDiff(TreeNode root) {
+
+//         findMaxDiff(root);
+//         return maxDiff;
+//     }
+// }
+
+
+// --------------------- One Pass Solution -----------------------
+
+class Solution{
+    private int dfs(TreeNode root, int mx, int mn){
+        if(root == null) return mx - mn;
+        mx = Math.max(mx, root.val);
+        mn = Math.min(mn, root.val);
+        return Math.max(dfs(root.left, mx, mn), dfs(root.right, mx, mn));
     }
 
-    void findMaxDiff(TreeNode root){
-        if(root == null) return;
-
-        findMaxUtil(root, root.left);
-        findMaxUtil(root, root.right);
-
-        findMaxDiff(root.left);
-        findMaxDiff(root.right);
-
-    } 
-
     public int maxAncestorDiff(TreeNode root) {
-
-        findMaxDiff(root);
-        return maxDiff;
+        return dfs(root, root.val, root.val);
     }
 }

ff996686e4460a7e96afd9e064f20c1f9c08778b
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-12 10:25:32 +0530

    Time: 1 ms (100.00%) | Memory: 41.8 MB (24.13%) - LeetSync

--- LeetcodeJan/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ LeetcodeJan/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,26 +1,52 @@
+// class Solution {
+
+//     private boolean isVolwel(char ch){
+//         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+//     }
+//     public boolean halvesAreAlike(String s) {
+//         int n = s.length();
+//         int countLeft = 0;
+//         int countRight = 0;
+//         for(int i = 0; i < n/2; i++){
+//             if(isVolwel(s.charAt(i))){
+//                 countLeft++;
+//             }
+//         }
+
+//         for(int i = n/2; i < n; i++){
+//             if(isVolwel(s.charAt(i))){
+//                 countRight++;
+//             }
+//         }
+//         if(countRight == countLeft){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
+
+// ---------------------- another approach ------------------------
+
 class Solution {
-
-    private boolean isVolwel(char ch){
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+     public boolean halvesAreAlike(String s) {
+        int mid = s.length() / 2;
+        String firstHalf = s.substring(0, mid);
+        String secondHalf = s.substring(mid);
+        
+        int countFirst = countVowels(firstHalf);
+        int countSecond = countVowels(secondHalf);
+        
+        return countFirst == countSecond;
     }
-    public boolean halvesAreAlike(String s) {
-        int n = s.length();
-        int countLeft = 0;
-        int countRight = 0;
-        for(int i = 0; i < n/2; i++){
-            if(isVolwel(s.charAt(i))){
-                countLeft++;
+    
+    private int countVowels(String str) {
+        int count = 0;
+        for (char ch : str.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(ch) != -1) {
+                count++;
             }
         }
-
-        for(int i = n/2; i < n; i++){
-            if(isVolwel(s.charAt(i))){
-                countRight++;
-            }
-        }
-        if(countRight == countLeft){
-            return true;
-        }
-        return false;
+        return count;
     }
 }

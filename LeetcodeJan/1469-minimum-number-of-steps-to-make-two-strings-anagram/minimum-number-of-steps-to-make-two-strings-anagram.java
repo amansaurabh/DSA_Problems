b6b8f2f74af006fa187d92ff250d3b47cc3dfaf0
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-13 09:07:53 +0530

    Time: 6 ms (99.74%) | Memory: 45.3 MB (18.05%) - LeetSync

--- LeetcodeJan/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ LeetcodeJan/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,22 +1,45 @@
 class Solution {
     public int minSteps(String s, String t) {
-        Map<Character, Integer> countMap = new HashMap<>();
+        // Map<Character, Integer> countMap = new HashMap<>();
 
-        // Update counts for string s
-        for (char c : s.toCharArray()) {
-            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        // // Update counts for string s
+        // for (char c : s.toCharArray()) {
+        //     countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        // }
+
+        // // Update counts for string t and calculate absolute differences
+        // int steps = 0;
+        // for (char c : t.toCharArray()) {
+        //     countMap.put(c, countMap.getOrDefault(c, 0) - 1);
+        //     if (countMap.get(c) < 0) {
+        //         // Character not present in s or extra occurrences in t
+        //         steps++;
+        //     }
+        // }
+
+        // return steps;
+
+        // ----------- using array to store ----------------------------
+
+
+        int[] charFreq = new int[26];
+
+        for(char cur: s.toCharArray()) {
+            charFreq[cur-'a']++;
         }
 
-        // Update counts for string t and calculate absolute differences
-        int steps = 0;
-        for (char c : t.toCharArray()) {
-            countMap.put(c, countMap.getOrDefault(c, 0) - 1);
-            if (countMap.get(c) < 0) {
-                // Character not present in s or extra occurrences in t
-                steps++;
+        for(char cur: t.toCharArray()) {
+            charFreq[cur-'a']--;
+        }
+
+        int minSteps = 0;
+
+        for(int idx=0; idx < 26; idx++) {
+            if (charFreq[idx] > 0) {
+                minSteps += charFreq[idx];
             }
         }
 
-        return steps;
+        return minSteps;
     }
 }

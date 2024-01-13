4780f0549c24521617715161e1efd9c353aedd12
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-13 19:29:45 +0530

    Time: 17 ms (61.34%) | Memory: 44.8 MB (5.21%) - LeetSync

--- linkedList/147-insertion-sort-list/insertion-sort-list.java
+++ linkedList/147-insertion-sort-list/insertion-sort-list.java
@@ -10,22 +10,58 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ListNode curr = head.next;
-        ListNode temp = head;
 
-        while(curr != null){
-            while(temp != curr){
-                if(temp.val > curr.val){
-                    //swap
-                    int temp1 = temp.val;
-                    temp.val = curr.val;
-                    curr.val = temp1;
-                }
-                temp = temp.next;
-            }
-            temp = head;
-            curr = curr.next;
+        // ---------------- Brute force -----------------
+
+        // ListNode curr = head.next;
+        // ListNode temp = head;
+
+        // while(curr != null){
+        //     while(temp != curr){
+        //         if(temp.val > curr.val){
+        //             //swap
+        //             int temp1 = temp.val;
+        //             temp.val = curr.val;
+        //             curr.val = temp1;
+        //         }
+        //         temp = temp.next;
+        //     }
+        //     temp = head;
+        //     curr = curr.next;
+        // }
+        // return head;
+
+        if(head == null || head.next == null){
+            return head;
         }
-        return head;
+        
+        ListNode sorted = null;
+        
+        while(head != null){
+            ListNode current = head;
+            head = head.next;
+            current.next = null;
+            
+            sorted = insertIntoSorted(sorted, current);
+        }
+        
+        return sorted;
+    }
+
+    private static ListNode insertIntoSorted(ListNode sorted, ListNode node){
+        if(sorted == null || node.val <= sorted.val){
+            node.next = sorted;
+            return node;
+        }
+        
+        ListNode current = sorted;
+        while(current.next != null && current.next.val < node.val){
+            current = current.next;
+        }
+        
+        node.next = current.next;
+        current.next = node;
+        
+        return sorted;
     }
 }

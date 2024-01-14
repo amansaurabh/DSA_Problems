172559826f914d69ffe3c2d669d20ed40050015e
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-14 16:44:23 +0530

    Time: 1 ms (98.70%) | Memory: 43.3 MB (86.82%) - LeetSync

--- stack/1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ stack/1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,28 +1,46 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
+        // int n = prices.length;
+
+        // Stack<Integer> stk = new Stack<>();
+        // int[] ans = new int[n];
+
+        // for(int i = n - 1; i >= 0; i--){
+        // if(stk.isEmpty()){
+        // ans[i] = prices[i];
+        // }else if(stk.peek() < prices[i]){
+
+        // ans[i] = prices[i] - stk.peek();
+        // }else{
+        // while(stk.size() > 0 && stk.peek() > prices[i]){
+        // stk.pop();
+        // }
+        // if(stk.isEmpty()){
+        // ans[i] = prices[i];
+        // }
+        // else if(stk.peek() < prices[i]){
+        // ans[i] = prices[i] - stk.peek();
+        // }
+        // }
+        // stk.push(prices[i]);
+        // }
+        // return ans;
+
+
+        // ------------------------- Brute force -------------------------
+        int j = 0, i = 0;
         int n = prices.length;
-
-        Stack<Integer> stk = new Stack<>();
-        int[] ans = new int[n];
-
-        for(int i = n - 1; i >= 0; i--){
-            if(stk.isEmpty()){
-                ans[i] = prices[i];
-            }else if(stk.peek() < prices[i]){
-                
-                ans[i] = prices[i] - stk.peek();
-            }else{
-                while(stk.size() > 0 && stk.peek() > prices[i]){
-                    stk.pop();
-                }
-                if(stk.isEmpty()){
-                    ans[i] = prices[i];
-                }
-                else if(stk.peek() < prices[i]){
-                    ans[i] = prices[i] - stk.peek();
+        int ans[] = new int[n];
+        for (i = 0; i < n; i++) {
+            for (j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    ans[i] = prices[i] - prices[j];
+                    break;
                 }
             }
-            stk.push(prices[i]);
+            if (j == prices.length) {
+                ans[i] = prices[i];
+            }
         }
         return ans;
     }

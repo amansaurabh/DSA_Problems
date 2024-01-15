fed53465b96de2b4d541355a155fe3731f6b0aa7
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-15 11:25:32 +0530

    Time: 92 ms (39.47%) | Memory: 107.6 MB (17.84%) - LeetSync

--- LeetcodeJan/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ LeetcodeJan/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -8,7 +8,10 @@ class Solution {
             lost.put(matches[i][1], lost.getOrDefault(matches[i][1], 0) + 1);
         }
 
-        List<Integer> notLost = new ArrayList<>();
+        // List<Integer> notLost = new ArrayList<>();
+        // 0R
+        Set<Integer> notLost = new HashSet<>();
+
         List<Integer> oneLost = new ArrayList<>();
 
         for(int i = 0; i < n; i++){
@@ -17,7 +20,7 @@ class Solution {
 
             if(!lost.containsKey(winner)){
                 notLost.add(winner);
-                lost.put(winner, 2);
+                // lost.put(winner, 2);
             }
 
             if(lost.get(loser) == 1){
@@ -25,11 +28,18 @@ class Solution {
             }  
         }
 
-        Collections.sort(notLost);
+
+        
+        List<Integer> newNotLost = new ArrayList<>(notLost);
+
+
+        // Collections.sort(notLost);
+        Collections.sort(newNotLost);
         Collections.sort(oneLost);
 
         List<List<Integer>> result = new ArrayList<>();
-        result.add(notLost);
+        // result.add(notLost);
+        result.add(newNotLost);
         result.add(oneLost);
 
         return result;

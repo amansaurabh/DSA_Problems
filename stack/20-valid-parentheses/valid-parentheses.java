a8f24775662bab3c376ba623ea031dae2df0b131
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-15 23:32:55 +0530

    Time: 1 ms (98.64%) | Memory: 41.6 MB (12.62%) - LeetSync

---
+++ stack/20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stk.push(ch);
+            } else{
+                if(stk.isEmpty()){
+                    return false;
+                }
+                char top = stk.pop();
+                if ((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stk.isEmpty();
+    }
+}

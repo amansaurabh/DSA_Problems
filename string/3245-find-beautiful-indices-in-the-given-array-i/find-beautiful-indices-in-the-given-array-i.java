c1d7fb006ad2bd424d97728eb1d48fe3f198dd0e
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-16 16:50:43 +0530

    Time: 15 ms (88.04%) | Memory: 45.3 MB (99.19%) - LeetSync

--- string/3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.java
+++ string/3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.java
@@ -1,25 +1,61 @@
 class Solution {
     public List<Integer> beautifulIndices(String s, String a, String b, int k) {
-         List<Integer> i_index = new ArrayList<>();
-         List<Integer> j_index = new ArrayList<>();
-        
-        
-        i_index = KMP(s, a);
-        j_index = KMP(s, b);
+
+        List<Integer> i_indices = KMP(s, a);
+        List<Integer> j_indices = KMP(s, b);
         
         List<Integer> result = new ArrayList<>();
         
-        for(int i = 0; i < i_index.size(); i++){
-            for(int j = 0; j < j_index.size(); j++){
-                int absDiff = Math.abs(i_index.get(i) - j_index.get(j));
-                if(absDiff <= k){
-                    result.add(i_index.get(i));
-                    break;
-                }
+        //it's O(n2)
+
+        // for(int i = 0; i < i_index.size(); i++){
+        //     for(int j = 0; j < j_index.size(); j++){
+        //         int absDiff = Math.abs(i_index.get(i) - j_index.get(j));
+        //         if(absDiff <= k){
+        //             result.add(i_index.get(i));
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // better method to do the same
+        // when will open |16 - j| <= k 
+        // then wil be become 16 - j <= k and 16 - j >= -k
+        // so we have to check these condition only
+
+        for (int i : i_indices) {
+            /*
+             * For every index i where 'a' was found in 's' I will see if we find any index
+             * j in the range i-k and i+k
+             */
+
+            int left_limit = Math.max(0, i - k); // To avoid out of bound -> I used max(0, i-k)
+            int right_limit = Math.min(s.length() - 1, i + k); // To avoid out of bound -> I used min(n-1, i+k)
+
+            int lowerBoundIndex = lowerBound(j_indices, left_limit);
+
+            if (lowerBoundIndex < j_indices.size() && j_indices.get(lowerBoundIndex) <= right_limit) {
+                result.add(i);
             }
         }
-        
-        Collections.sort(result);
+
+        return result;
+    }
+
+    private int lowerBound(List<Integer> list, int target) {
+        int left = 0, right = list.size() - 1, result = list.size();
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (list.get(mid) >= target) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
         return result;
     }
     

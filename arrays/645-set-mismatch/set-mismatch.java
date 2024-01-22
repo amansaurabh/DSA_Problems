5d4ad49c5d7aa0b8b247fd8abe6ee99c91488ac6
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-22 13:02:28 +0530

    Time: 2 ms (97.74%) | Memory: 45.7 MB (23.87%) - LeetSync

--- arrays/645-set-mismatch/set-mismatch.java
+++ arrays/645-set-mismatch/set-mismatch.java
@@ -1,25 +1,37 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n =  nums.length;
+        // int n =  nums.length;
 
-        int dup = -1;
-        int missing = -1;
+        // int dup = -1;
+        // int missing = -1;
 
-        for(int i = 0; i < n; i++){
-            int num = Math.abs(nums[i]);
+        // for(int i = 0; i < n; i++){
+        //     int num = Math.abs(nums[i]);
 
-            if(nums[num - 1] < 0){
-                dup = num;
-            }else{
-                nums[num - 1] *= (-1);
-            }
+        //     if(nums[num - 1] < 0){
+        //         dup = num;
+        //     }else{
+        //         nums[num - 1] *= (-1);
+        //     }
+        // }
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] > 0){
+        //         missing = (i + 1);
+        //         break;
+        //     }
+        // }
+        // return new int[]{dup, missing};
+
+        int N = nums.length, sum = N * (N + 1) / 2;
+        int[] result = new int[2];
+        boolean[] seen = new boolean[N+1];
+        for (int num : nums) {
+            sum -= num;
+            if (seen[num]) result[0] = num;
+            seen[num] = true;
         }
-        for(int i = 0; i < n; i++){
-            if(nums[i] > 0){
-                missing = (i + 1);
-                break;
-            }
-        }
-        return new int[]{dup, missing};
+        result[1] = sum + result[0];
+        return result;
+        
     }
 }

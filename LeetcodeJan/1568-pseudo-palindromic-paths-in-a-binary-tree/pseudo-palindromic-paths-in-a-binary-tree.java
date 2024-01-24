ffee0568ba0a2177eb16b8583d600ef2e907c8e2
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-24 12:01:05 +0530

    Time: 16 ms (49.11%) | Memory: 70.1 MB (46.97%) - LeetSync

---
+++ LeetcodeJan/1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int count = 0;
+    public int pseudoPalindromicPaths (TreeNode root) {
+        int[] freq = new int[10];
+        solve(root, freq);
+        return count;
+    }
+
+    private void solve(TreeNode root, int[] temp){
+        if(root != null){
+            temp[root.val]++;
+
+            if(root.left == null && root.right == null){
+                int oddFreq = 0;
+                for(int i = 1; i < 10; i++){
+                    if(temp[i] % 2 != 0){
+                        oddFreq++;
+                    }
+                }
+                count += oddFreq <= 1? 1 : 0;
+            }
+            solve(root.left, temp);
+            solve(root.right, temp);
+
+            temp[root.val]--;
+        }
+    }
+}

fb2616802ab2aeea2cbc974bec63a353168eefd7
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-01-24 12:05:35 +0530

    Time: 6 ms (96.09%) | Memory: 69.3 MB (76.16%) - LeetSync

--- LeetcodeJan/1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
+++ LeetcodeJan/1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -16,28 +16,47 @@
 class Solution {
     private int count = 0;
     public int pseudoPalindromicPaths (TreeNode root) {
-        int[] freq = new int[10];
-        solve(root, freq);
+        // int[] freq = new int[10];
+        // solve(root, freq);
+        // return count;
+
+        // -------- Another method using Bit -----------
+        solve(root, 0);
         return count;
     }
 
-    private void solve(TreeNode root, int[] temp){
+    // private void solve(TreeNode root, int[] temp){
+    //     if(root != null){
+    //         temp[root.val]++;
+
+    //         if(root.left == null && root.right == null){
+    //             int oddFreq = 0;
+    //             for(int i = 1; i < 10; i++){
+    //                 if(temp[i] % 2 != 0){
+    //                     oddFreq++;
+    //                 }
+    //             }
+    //             count += oddFreq <= 1? 1 : 0;
+    //         }
+    //         solve(root.left, temp);
+    //         solve(root.right, temp);
+
+    //         temp[root.val]--;
+    //     }
+    // }
+
+
+    private void solve(TreeNode root, int temp){
         if(root != null){
-            temp[root.val]++;
+            temp = temp ^ (1 << root.val);
 
             if(root.left == null && root.right == null){
-                int oddFreq = 0;
-                for(int i = 1; i < 10; i++){
-                    if(temp[i] % 2 != 0){
-                        oddFreq++;
-                    }
+                if((temp & (temp - 1)) == 0){
+                    count++;
                 }
-                count += oddFreq <= 1? 1 : 0;
             }
             solve(root.left, temp);
             solve(root.right, temp);
-
-            temp[root.val]--;
         }
     }
 }

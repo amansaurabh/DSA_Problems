4b49f8200e98cb616a0d6b3ccd4322b4efcfb095
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-02-14 19:47:34 +0530

    Time: 43 ms (69.66%) | Memory: 45.5 MB (20.29%) - LeetSync

--- graphs/1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ graphs/1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,31 +1,38 @@
 class Solution {
-    int[] DIR = new int[]{0, 1, 0, -1, 0};
-
     public int minimumEffortPath(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
-        int[][] dist = new int[m][n];
-        for (int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-        
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minHeap.offer(new int[]{0, 0, 0}); // distance, row, col
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        int[][] dist = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        minHeap.add(new int[]{0, 0, 0});
         dist[0][0] = 0;
+        
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         
         while (!minHeap.isEmpty()) {
             int[] top = minHeap.poll();
             int d = top[0], r = top[1], c = top[2];
-            if (d > dist[r][c]) continue; // this is an outdated version -> skip it
-            if (r == m - 1 && c == n - 1) return d; // Reach to bottom right
-            for (int i = 0; i < 4; i++) {
-                int nr = r + DIR[i], nc = c + DIR[i + 1];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+            
+            if (d > dist[r][c]) continue;
+            
+            if (r == rows - 1 && c == cols - 1) return d;
+            
+            for (int[] dir : directions) {
+                int nr = r + dir[0], nc = c + dir[1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
                     int newDist = Math.max(d, Math.abs(heights[nr][nc] - heights[r][c]));
-                    if (dist[nr][nc] > newDist) {
+                    if (newDist < dist[nr][nc]) {
                         dist[nr][nc] = newDist;
-                        minHeap.offer(new int[]{dist[nr][nc], nr, nc});
+                        minHeap.add(new int[]{newDist, nr, nc});
                     }
                 }
             }
         }
-        return 0; // Unreachable code, Java require to return interger value.
+        return -1;
     }
 }

29205cdba8ecdee69c088d4025d44249d35d5194
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-02-17 23:21:19 +0530

    Time: 2 ms (97.62%) | Memory: 44.9 MB (34.08%) - LeetSync

--- arrays/1-two-sum/two-sum.java
+++ arrays/1-two-sum/two-sum.java
@@ -1,38 +1,32 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        //using 2 pointer
-        
-        //  int left = 0 , right = nums.length - 1 , tempSum;
-        // while(left < right)
-        // {
-        //     tempSum = nums[left] + nums[right];
-        //     if(tempSum == target)
-        //         return new int[]{left, right};
-        //     if(tempSum > target)
-        //         right--;
-        //     else
-        //         left++;
-        // }
-        // return new int[]{right , left};
-        // for(int i = 0 ; i < nums.length - 1 ; i++)
-        //     for(int j = i + 1 ; j < nums.length ; j++)
-        //     {
-        //         if(nums[i] + nums[j] == target)
-        //             return new int[]{i, j };
-        //     }
-        // return new int[]{-1 , -1};
-        
-         HashMap<Integer,Integer> indexMap = new HashMap<Integer,Integer>();
-        for(int i = 0; i < nums.length; i++){
-            Integer requiredNum = (Integer)(target - nums[i]);
-            if(indexMap.containsKey(requiredNum)){
-                int toReturn[] = {indexMap.get(requiredNum), i};
-                return toReturn;
-            }
+        // Approach 1 : BRUTE FORCE O(n^2)
+        // int n = nums.length;
+        // int[] res = new int[2];
 
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             res[0] = i;
+        //             res[1] = j;
+        //             return res;
+        //         }
+        //     }
+        // }
+        // return new int[0];
+
+        // Approach 2 :
+
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int reqNum = target - nums[i];
+            if(indexMap.containsKey(reqNum)){
+                int[] res = {indexMap.get(reqNum), i};
+                return res;
+            }
             indexMap.put(nums[i], i);
         }
-        return null;
+        return new int[0];
     }
 }

0b1551148e6ffff8a63119fdbecb643a3568d712
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-02-18 14:32:53 +0530

    Time: 3 ms (99.43%) | Memory: 45.7 MB (88.63%) - LeetSync

--- arrays/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ arrays/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -39,26 +39,46 @@ class Solution {
 
         // Using Map
 
-        int n = nums.length;
-        //{remainder, times}
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
-        map.put(0, 1);
+        // int n = nums.length;
+        // //{remainder, times}
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int sum = 0;
+        // map.put(0, 1);
 
+        // int result = 0;
+
+        // for(int i = 0; i < n; i++){
+        //     sum += nums[i];
+
+        //     int rem = sum % k;
+
+        //     if(rem < 0) rem += k;
+
+        //     if(map.containsKey(rem)){
+        //         result += map.get(rem);
+        //     }
+
+        //     map.put(rem, map.getOrDefault(rem, 0) + 1);
+        // }
+        // return result;
+
+        // Using Array
+
+        int[] map = new int[k];
         int result = 0;
+        int sum = 0;
+        map[0] = 1;
 
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
+        for(int num : nums){
+            sum += num;
 
             int rem = sum % k;
 
             if(rem < 0) rem += k;
 
-            if(map.containsKey(rem)){
-                result += map.get(rem);
-            }
+            result += map[rem];
 
-            map.put(rem, map.getOrDefault(rem, 0) + 1);
+            map[rem]++;
         }
         return result;
     }

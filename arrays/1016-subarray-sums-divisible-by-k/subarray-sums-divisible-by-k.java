ed4142db956db4cfe5356864269181208bae853e
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-02-18 14:25:01 +0530

    Time: 25 ms (42.02%) | Memory: 46.8 MB (75.50%) - LeetSync

--- arrays/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ arrays/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -16,23 +16,50 @@ class Solution {
         // }
         // return count;
 
+
+        // int n = nums.length;
+        // int count = 0;
+        // int[] prefixSum = new int[n + 1];
+        // prefixSum[0] = 0;
+
+        // for(int i = 1; i <= n; i++){
+        //     prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        // }
+
+        // int[] remainderCount = new int[k];
+        // remainderCount[0] = 1;
+
+        // for(int i = 1; i <= n; i++){
+        //     int remainder = (prefixSum[i] % k + k) % k; // Ensure positive remainder
+        //     count += remainderCount[remainder];
+        //     remainderCount[remainder]++;
+        // }
+        // return count;
+
+
+        // Using Map
+
         int n = nums.length;
-        int count = 0;
-        int[] prefixSum = new int[n + 1];
-        prefixSum[0] = 0;
+        //{remainder, times}
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, 1);
 
-        for(int i = 1; i <= n; i++){
-            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        int result = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+
+            int rem = sum % k;
+
+            if(rem < 0) rem += k;
+
+            if(map.containsKey(rem)){
+                result += map.get(rem);
+            }
+
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
         }
-
-        int[] remainderCount = new int[k];
-        remainderCount[0] = 1;
-
-        for(int i = 1; i <= n; i++){
-            int remainder = (prefixSum[i] % k + k) % k; // Ensure positive remainder
-            count += remainderCount[remainder];
-            remainderCount[remainder]++;
-        }
-        return count;
+        return result;
     }
 }

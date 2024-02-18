a1f98f39f2716f7b33f5d222e36f3023b8f66ea2
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-02-18 18:28:15 +0530

    Time: 7 ms (50.83%) | Memory: 53.3 MB (90.30%) - LeetSync

--- arrays/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ arrays/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,19 +1,38 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        int n = nums.length;
-        int[] freq = new int[n + 1];
+        // int n = nums.length;
+        // int[] freq = new int[n + 1];
 
-        for(int num : nums){
-            freq[num]++;
-        }
+        // for(int num : nums){
+        //     freq[num]++;
+        // }
+
+        // List<Integer> ans = new ArrayList<>();
+
+        // for(int i = 0; i < n + 1; i++){
+        //     if(freq[i] == 2){
+        //         ans.add(i);
+        //     }
+        // }
+        // return ans;
 
         List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
 
-        for(int i = 0; i < n + 1; i++){
-            if(freq[i] == 2){
-                ans.add(i);
+        for(int i = 0; i < n; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] < 0){
+                ans.add(Math.abs(nums[i]));
             }
+            nums[index] = -nums[index];
         }
+
+        // Reset the array back to its original values
+        // not mandatory to do this , but good to do during inteview
+        for (int i = 0; i < n; i++) {
+            nums[i] = Math.abs(nums[i]);
+        }
+
         return ans;
     }
 }

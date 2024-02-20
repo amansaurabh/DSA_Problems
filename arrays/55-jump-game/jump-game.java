844a0c6177dea1e4b3b274e7eaefd0fc117db110
amansaurabh <75749961+amansaurabh@users.noreply.github.com>
2024-02-20 12:58:08 +0530

    Time: 1 ms (99.97%) | Memory: 45.5 MB (74.52%) - LeetSync

---
+++ arrays/55-jump-game/jump-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        // int reachable = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(i > reachable) return false;
+        //     reachable = Math.max(reachable, i + nums[i]);
+        // }
+        // return true;
+
+
+        //Intially final position is the last index
+        int finalPosition = nums.length - 1;
+
+        // Start from second last index
+        for(int i = nums.length - 2; i >= 0; i--){
+            //If u reach the final position from this index
+            // update the final position flag and check from indicies
+
+            if(nums[i] >= finalPosition - i){
+                finalPosition = i;
+            }
+        }
+        return finalPosition == 0;
+    }
+}

1c183764756d4ec373d9ad518d506cdf3f631877
Aman Saurabh <75749961+amansaurabh@users.noreply.github.com>
2024-04-17 00:55:32 +0530

    Time: 5 ms (42.85%) | Memory: 45 MB (84.68%) - LeetSync

---
+++ arrays/137-single-number-ii/single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for(int i = 1; i < n; i = i + 3){
+            if(nums[i] != nums[i - 1] || nums[i] != nums[i + 1]){
+                return nums[i - 1];
+            }
+        }
+        return nums[n - 1];
+    }
+}
